3c130fc1e36e4c8f7f6f63f232e7431543b534a6
armaBurton <armanarma@armaburton.com>
2022-05-16 14:47:08 -0700

    everything is working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import styles from './Header.css';
+import { useEntries } from '../../context/PlannerContext';
 
 export default function Header() {
+  const { entries } = useEntries();
+  console.log(`|| entries >`, entries);
+
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
       {/* TODO: Replace hard-coded count with the actual number of entries */}
-      <span>1 Item Left</span>
+      <span>{entries.length} Item Left</span>
     </header>
   );
 }
